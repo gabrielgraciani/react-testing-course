0772aaa26f5ccb3f3964da48c484bb11aaa40eb7
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2021-03-25 20:27:47 -0300

    test and code button click

--- 01-color-button/src/App.test.js
+++ 01-color-button/src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 test('button has correct initial color', () => {
@@ -9,12 +9,17 @@ test('button has correct initial color', () => {
 
   // expect the background color to be red
   expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
+  
+  //blick button
+  fireEvent.click(colorButton);
+  
+  // expect the background color to be blue
+  expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
+
+  // expect the button text to be 'Change to red'
+  expect(colorButton.textContent).toBe('Change to red');
 });
 
 // test('button has correct initial text', () => {
 
 // });
-
-test('button turns blue when clicked', () => {
-
-});
